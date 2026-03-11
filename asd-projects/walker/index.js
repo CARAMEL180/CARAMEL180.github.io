06bc8954023ad5a2b56df0bd94d8abf7f1b79e06
carmenVverdin <cvverdinreal@gmail.com>
2026-03-11 16:50:28 +0000

    fix

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,7 +10,7 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+
   // Game Item Objects
 
 
@@ -23,8 +23,24 @@ function runProgram(){
 
   Note: You can have multiple event listeners for different types of events.
   */
-  $(document).on('eventType', handleEvent);                          
 
+  const walker = {
+    x: 0,
+    y: 0,
+    speedX: 0,
+    speedY: 0,
+  };
+
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp);
+
+  const KEY = {
+    ENTER: 13,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+  };
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -34,25 +50,55 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem();
+    redrawGameItem();
   }
-  
+
   /* 
   This section is where you set up the event handlers for user input.
   For example, if you wanted to make an event handler for a click event, you should rename this function to 'handleClick', then write the code that should execute when the click event occurs.
   
   Note: You can have multiple event handlers for different types of events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    if (event.which === KEY.LEFT) {
+      walker.speedX -= 5;
+      console.log("left pressed");
+    } else if (event.which === KEY.UP) {
+      walker.speedY -= 5;
+      console.log("up pressed")
+    } else if (event.which === KEY.RIGHT) {
+      walker.speedX += 5;
+      console.log("right pressed")
+    } else if (event.which === KEY.DOWN) {
+      walker.speedY += 5;
+      console.log("down pressed")
+    }
+    console.log(event.which);
+  }
 
+  function handleKeyUp(event) {
+    if (event.which === KEY.LEFT) {
+      walker.speedX = 0;
+      console.log("left released");
+    } else if (event.which === KEY.UP) {
+      walker.speedY = 0;
+      console.log("up released")
+    } else if (event.which === KEY.RIGHT) {
+      walker.speedX = 0;
+      console.log("right released")
+    } else if (event.which === KEY.DOWN) {
+      walker.speedY = 0;
+      console.log("down released")
+    }
+    console.log(event.which);
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -60,5 +106,19 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
+  function repositionGameItem() {
+    walker.x += walker.speedY;
+    walker.y += walker.speedX;
+
+    console.log(walker.x, walker.y);
+  }
+
+  function redrawGameItem() {
+    $("#walker").css("left", walker.x);
+    $("#walker").css("top", walker.y);
+
+    console.log("Walker position:", walker.x, walker.y);
+  }
+
 }
